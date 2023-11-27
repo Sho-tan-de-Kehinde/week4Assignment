b562e3acae97b86f055cb996753418fd9275a230
Shotande Kehinde <hussienk2011@gmail.com>
2023-11-27 20:31:42 +0100

    week4

---
+++ script.js
@@ -0,0 +1,68 @@
+// alert("Welcome to CWW Tech Cohort-5")
+//  Number variable type 
+/*let X = 20;
+let Y = 10;
+// Arithmetic operations(Addition,subtractions,Multiplication and division) 
+console.log(X + Y);
+console.log(X - Y);
+console.log(X / Y);
+console.log(X * Y);*/
+
+//  String variable type
+/*let A = "10";
+let B = 2;
+// Arithmetic operations(Addition,subtractions,Multiplication and division)
+console.log(A + B);
+console.log(A - B);
+console.log(A / B);
+console.log(A * B);*/
+
+//  Boolean variable type
+/*et X = 18;
+let Y = 20;
+// Arithmetic operations(Addition,subtractions,Multiplication and division)
+console.log(X + Y == 38);
+console.log(X - Y == 38);
+console.log(X / Y == 0);
+console.log(X * Y == 360);*/
+
+
+// If- Else tatement
+/*const num = 3;
+if((num % 2) === 0 ){
+    console.log("Yoy have entered an even number")
+}
+else{
+    console.log("Yoy have entered an odd number")
+}*/
+
+
+    // FOR Loop
+ /*const num = 0;
+for(let num = 1; num <= 10 ; num++) {
+    console.log(num);
+}  */
+
+//    Function in JavaScript
+/*function Addition(A , B) {
+    return A + B;
+}
+console.log(Addition(100 , 200));*/
+
+// Array Declaration
+//  const array = ["Mango", "Orange", "Strawberry", "grapes", "Pineapple"];
+
+// object declaration
+/*const person = {
+    Name: "John",
+    Age: "16",
+    Hobby: "Reading",  
+}*/
+
+// A Higher Order function
+let randomNumber = [1, 3, 4, 5, 8, 12]
+ let newRandomNumber = randomNumber.map((randomNumber) => {
+    return randomNumber + 2;
+});
+  console.log(newRandomNumber);
+
